ef2e5ed4ba89fabeece52e169d7b783f5c40728c
Kitty199203 <82276776+KittyC513@users.noreply.github.com>
2021-04-19 18:58:14 -0400

    Set up the array of dice
    randomColor and value

---
+++ D6_Unity/Assets/Scripts/Dice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    private Color[] colors = new Color[] { Color.red, Color.green, Color.blue, Color.cyan, Color.grey, Color.yellow };
+    public int colourIndex;
+    public int faceValue;
+    
+    public int positionInRow;
+    public bool isOnBottomRow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colourIndex = Random.Range(0, 6);
+        GetComponent<SpriteRenderer>().color = colors[colourIndex];
+        faceValue = Random.Range(1, 7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ D6_Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] topSlots = new GameObject[5];
+    public GameObject[] bottomSlots = new GameObject[5];
+    public GameObject[] DicePrefab = new GameObject[5];
+   
+    Dice[] topRow = new Dice[5];
+    Dice[] bottomRow = new Dice[5];
+    Dice currentDie;
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        for (int i = 0; i < topRow.Length; i++)
+        {
+            topRow[i] = Instantiate(DicePrefab[i], topSlots[i].transform.position,Quaternion.identity).GetComponent<Dice>();
+            bottomRow[i] = Instantiate(DicePrefab[i], bottomSlots[i].transform.position, Quaternion.identity).GetComponent<Dice>();
+
+            topRow[i].isOnBottomRow = false;
+            bottomRow[i].isOnBottomRow = true;
+
+            topRow[i].positionInRow = i;
+            bottomRow[i].positionInRow = i;
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
